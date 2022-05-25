2f53b1d679bdac4defd8ab48d25ad5feece27c38
Fabian Affolter <mail@fabian-affolter.ch>
2022-05-25 22:36:26 +0200

    python39Packages.xarray-einstats: init at 0.2.2
    (cherry picked from commit a7f104763d978514f7a32a974a4dd558f56a6279)

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -11146,6 +11146,8 @@ in {
 
   xarray = callPackage ../development/python-modules/xarray { };
 
+  xarray-einstats = callPackage ../development/python-modules/xarray-einstats { };
+
   xattr = callPackage ../development/python-modules/xattr { };
 
   xbox-webapi = callPackage ../development/python-modules/xbox-webapi { };
