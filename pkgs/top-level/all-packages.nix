5cd3cedee7cb914f796d1d7a2355ed546afd71ea
Alexander Bantyev <balsoft@balsoft.ru>
2022-05-29 18:47:07 +0400

    sonic-lineup: stdenv -> gcc10Stdenv
    (cherry picked from commit b9af2b5afded6f97f1c77e988fd759337e6935b0)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -29647,7 +29647,7 @@ with pkgs;
 
   socialscan = with python3.pkgs; toPythonApplication socialscan;
 
-  sonic-lineup = libsForQt5.callPackage ../applications/audio/sonic-lineup { };
+  sonic-lineup = libsForQt5.callPackage ../applications/audio/sonic-lineup { stdenv = gcc10StdenvCompat; };
 
   sonic-visualiser = libsForQt5.callPackage ../applications/audio/sonic-visualiser { };
 
